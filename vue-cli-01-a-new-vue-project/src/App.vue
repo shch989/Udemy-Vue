fd2ed0a74650089dcd39b3e486a178b8419adb25
shch989 <tankpop11@gmail.com>
2022-12-10 21:57:27 +0900

    2022-12-10

---
+++ vue-cli-01-a-new-vue-project/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div></div>
+</template>
+<script>
+const app = {
+  data() {
+    return {
+      friends: [
+        {
+          id: 'manuel',
+          name: 'Manuel Lorenz',
+          phone: '0123 45678 90',
+          email: 'manuel@localhost.com',
+        },
+        {
+          id: 'julie',
+          name: 'Julie Jones',
+          phone: '0987 654421 21',
+          email: 'julie@localhost.com',
+        },
+      ],
+    }
+  },
+}
+</script>
+<style></style>
